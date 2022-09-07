d5bebebb93e7d9a85e363a0a7aad6b50a6940564
Valera Bronshtein <valera@memphis.dev>
2022-09-08 00:14:38 +0300

    ingress change to lb svc

--- AWS/EKS/memphis-install.sh
+++ AWS/EKS/memphis-install.sh
@@ -3,5 +3,5 @@ echo $clusterid
 aws eks update-kubeconfig --name $clusterid
 helm repo add memphis https://k8s.memphis.dev/charts/
 helm install my-memphis memphis/memphis --create-namespace --namespace memphis
-kubectl apply -f memphis/ingress.yaml
-kubectl get ingress -n memphis
+kubectl apply -f memphis/svc.yaml
+kubectl get svc memphis-ui -n memphis
