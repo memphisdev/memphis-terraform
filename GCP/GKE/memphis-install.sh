b56395c32f3c5e499855b8ff857a6df8fcacf7f3
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-09-19 21:17:10 +0300

    Update memphis-install.sh

--- GCP/GKE/memphis-install.sh
+++ GCP/GKE/memphis-install.sh
@@ -2,7 +2,7 @@ gcloud container clusters get-credentials $(terraform output -raw kubernetes_clu
 helm repo add memphis https://k8s.memphis.dev/charts/
 helm install my-memphis memphis/memphis --create-namespace --namespace memphis
 kubectl apply -f memphis/svc.yaml
-until kubectl get pods --selector=app.kubernetes.io/name=memphis -o=jsonpath="{.items[*].status.phase}" -n memphis  | grep -q "Running" ; do sleep 1; done
+until kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis  | grep -q "<pending>" ; do sleep 1; done
 echo "To access Memphis using UI/CLI/SDK using service EXTERNAL-IP, run the below commands:"
 echo "Dashboard: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis):9000"
 echo "Memphis broker: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis):6666 (Client Connections)"
