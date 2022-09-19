7e43ba730588529f63f9572b7f6106f5efad3e6f
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-09-19 20:44:10 +0300

    Update memphis-install.sh

--- AWS/EKS/memphis-install.sh
+++ AWS/EKS/memphis-install.sh
@@ -4,5 +4,5 @@ aws eks update-kubeconfig --name $clusterid
 helm repo add memphis https://k8s.memphis.dev/charts/
 helm install my-memphis memphis/memphis --create-namespace --namespace memphis
 kubectl apply -f memphis/svc.yaml
-until kubectl get pods --selector=app=memphis-ui -o=jsonpath="{.items[*].status.phase}" -n memphis  | grep -q "Running" ; do sleep 1; done
-echo "Dashboard: http://$(kubectl get svc memphis-ui -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis)"
+until kubectl get pods --selector=app.kubernetes.io/name=memphis -o=jsonpath="{.items[*].status.phase}" -n memphis  | grep -q "Running" ; do sleep 1; done
+echo "Dashboard: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis)"
