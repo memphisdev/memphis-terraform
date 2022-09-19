42a0e79a29ca51fa4f75956bf9a37eee64e717be
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-09-19 21:55:51 +0300

    Update memphis-install.sh

--- AWS/EKS/memphis-install.sh
+++ AWS/EKS/memphis-install.sh
@@ -4,7 +4,9 @@ aws eks update-kubeconfig --name $clusterid
 helm repo add memphis https://k8s.memphis.dev/charts/
 helm install my-memphis memphis/memphis --create-namespace --namespace memphis
 kubectl apply -f memphis/svc.yaml
-until kubectl get pods --selector=app.kubernetes.io/name=memphis -o=jsonpath="{.items[*].status.phase}" -n memphis  | grep -q "Running" ; do sleep 1; done
+until kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis| grep -q ""; do sleep 1; done
+echo ""
+echo "---------------------------------------------------------------------------------------------------------------------------------------------"
 echo "To access Memphis using UI/CLI/SDK using service EXTERNAL-IP, run the below commands:"
 echo "Dashboard: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis):9000"
 echo "Memphis broker: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis):6666 (Client Connections)"
