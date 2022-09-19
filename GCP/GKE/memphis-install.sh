a0655a7f0599260cd9843b1eefac092b25117ff6
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-09-19 20:46:13 +0300

    Update memphis-install.sh

--- GCP/GKE/memphis-install.sh
+++ GCP/GKE/memphis-install.sh
@@ -2,5 +2,5 @@ gcloud container clusters get-credentials $(terraform output -raw kubernetes_clu
 helm repo add memphis https://k8s.memphis.dev/charts/
 helm install my-memphis memphis/memphis --create-namespace --namespace memphis
 kubectl apply -f memphis/svc.yaml
-until kubectl get pods --selector=app=memphis-ui -o=jsonpath="{.items[*].status.phase}" -n memphis  | grep -q "Running" ; do sleep 1; done
-echo "Dashboard: http://$(kubectl get svc memphis-ui -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis)"
+until kubectl get pods --selector=app.kubernetes.io/name=memphis -o=jsonpath="{.items[*].status.phase}" -n memphis  | grep -q "Running" ; do sleep 1; done
+echo "Dashboard: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis)"
