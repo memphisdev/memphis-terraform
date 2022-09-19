8b71e087b785ef7a7ea3a5bc38a94750a8a745ae
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-09-19 21:59:49 +0300

    Update memphis-install.sh

--- GCP/GKE/memphis-install.sh
+++ GCP/GKE/memphis-install.sh
@@ -5,6 +5,7 @@ kubectl apply -f memphis/svc.yaml
 until kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis| grep -q ""; do sleep 1; done
 echo ""
 echo "---------------------------------------------------------------------------------------------------------------------------------------------"
+echo ""
 echo "To access Memphis using UI/CLI/SDK using service EXTERNAL-IP, run the below commands:"
 echo "Dashboard: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis):9000"
 echo "Memphis broker: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n memphis):6666 (Client Connections)"
