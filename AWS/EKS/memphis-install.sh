f1d2cf1322d331a7ce6c60ed30d45512ebda0072
Valera <valera@memphis.dev>
2022-10-26 10:13:29 +0300

    benchmarks edit

--- AWS/EKS/memphis-install.sh
+++ AWS/EKS/memphis-install.sh
@@ -1,14 +1,4 @@
 clusterid=$(terraform output -raw cluster_id)
 echo $clusterid
 aws eks update-kubeconfig --name $clusterid
-helm repo add memphis https://k8s.memphis.dev/charts/
-helm install my-memphis memphis/memphis --create-namespace --namespace memphis
-kubectl apply -f memphis/svc.yaml
-until kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis| grep -q ""; do sleep 1; done
-echo ""
-echo "---------------------------------------------------------------------------------------------------------------------------------------------"
-echo ""
-echo "To access Memphis using UI/CLI/SDK with service EXTERNAL-IP, use the following URLs:"
-echo "Dashboard: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis):9000"
-echo "Memphis broker: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis):6666 (Client Connections)"
-echo "Memphis broker: http://$(kubectl get svc memphis-cluster-external -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}' -n memphis):9000 (CLI Connections)"
+helm install my-memphis ../../memphis-k8s/memphis --set analytics='false',cluster.enabled="true" --create-namespace --namespace memphis
