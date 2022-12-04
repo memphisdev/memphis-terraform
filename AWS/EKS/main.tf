299397e03b8b7d8f7e15f3c397b91c69761c877e
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-12-04 17:59:14 +0200

    Update main.tf

--- AWS/EKS/main.tf
+++ AWS/EKS/main.tf
@@ -14,3 +14,17 @@ resource "aws_kms_alias" "eks" {
   name          = "alias/${local.name_prefix}"
   target_key_id = aws_kms_key.eks.key_id
 }
+
+resource "helm_release" "metrics_server" {
+  # Name of the release in the cluster
+  name       = "metrics-server"
+
+  # Name of the chart to install
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+
+  # Version of the chart to use
+  chart      = "metrics-server"
+
+  # Wait for the release to be deployed
+  wait = true
+}
