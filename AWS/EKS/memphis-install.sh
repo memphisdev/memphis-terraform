55eb20d286296f0b8fa513ca73148a2c9c4620ac
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2023-04-24 20:22:22 +0300

    Update memphis-install.sh

--- AWS/EKS/memphis-install.sh
+++ AWS/EKS/memphis-install.sh
@@ -1,4 +1,4 @@
 clusterid=$(terraform output -raw cluster_id)
 echo $clusterid
 aws eks update-kubeconfig --name $clusterid
-helm install my-memphis ../../memphis-k8s/memphis --set analytics='false',cluster.enabled="true" --create-namespace --namespace memphis
+helm install my-memphis ../../memphis-k8s/memphis --set analytics='false',global.cluster.enabled="true" --create-namespace --namespace memphis
